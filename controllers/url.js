5d88a007a500fa8d7193bd648b93aaf07c78c125
Ved-Kulkarni <136430347+Ved-Kulkarni@users.noreply.github.com>
2025-07-01 05:00:15 +0530

    Update url.js

--- controllers/url.js
+++ controllers/url.js
@@ -1,31 +1,24 @@
-import { URL } from "../models/URL.js"
-import shortid from "shortid"
+import { URL } from "../models/URL.js";
+import shortid from "shortid";
 
-export const shortURL = async (req,res) => {
-  const longURL = req.body.longURL
-  const shortCode = shortid.generate()
+export const shortURL = async (req, res) => {
+  const longURL = req.body.longURL;
+  const shortCode = shortid.generate();
+  const shortURL = `${req.protocol}://${req.get("host")}/${shortCode}`;
 
-  const shortURL = `${req.protocol}://${req.get('host')}/${shortCode}`
+  const newURL = new URL({ shortCode, longURL });
+  await newURL.save();
 
-  // save to database
-  const newURL = new URL({shortCode, longURL})
-  await newURL.save()
-
-  console.log("Short URL saved as : ", newURL)
-
-  res.render("index.ejs", {shortURL})
-}
+  res.render("index.ejs", { shortURL });
+};
 
 export const getOriginalURL = async (req, res) => {
-  const shortCode = req.params.shortCode
+  const shortCode = req.params.shortCode;
+  const originalURL = await URL.findOne({ shortCode });
 
-  // find from database
-  const originalURL= await URL.findOne({shortCode})
-
-  if(originalURL) {
-    res.redirect(originalURL.longURL)
+  if (originalURL) {
+    res.redirect(originalURL.longURL);
+  } else {
+    res.status(404).send("Invalid or expired link");
   }
-  else {
-    res.json({message: "Invalid"})
-  }
-}
+};
