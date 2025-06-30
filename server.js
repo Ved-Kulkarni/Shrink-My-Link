59383bae615b1e8756cd02a566da8d2723ab5c98
Ved-Kulkarni <136430347+Ved-Kulkarni@users.noreply.github.com>
2025-07-01 04:18:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import { shortURL, getOriginalURL } from './controllers/url.js';
+import dotenv from "dotenv"
 
-mongoose.connect(
-  "mongodb+srv://vedkulkarni144:dI6n2kBCP6U6yh6I@cluster0.84r7ytm.mongodb.net/", {
-    dbName: "URL_SHORTENER"
-  }
-).then(() => console.log("MongoDB connected")).catch((error) => console.log(error));
+dotenv.config()
 
+mongoose.connect(process.env.MONGO_URI, {
+  dbName: "URL_SHORTENER"
+}).then(() => console.log("MongoDB connected")).catch((error) => console.log(error));
 
 const app = express()
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public"))
 
-const port = 4000
+const port = process.env.PORT || 4000
+
+app.set("view engine", "ejs")
+app.set("views", "./views")
+app.use(express.static("public"))
 
 // rendering the ejs file
 app.get('/', (req,res) => {
