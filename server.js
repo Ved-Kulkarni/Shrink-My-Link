38baf2a727a29e21c67ded331c68379d0e30ff34
Ved-Kulkarni <136430347+Ved-Kulkarni@users.noreply.github.com>
2025-07-01 04:59:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,31 @@
-import express from 'express'
-import mongoose from 'mongoose'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import { shortURL, getOriginalURL } from './controllers/url.js';
-import dotenv from "dotenv"
 
-dotenv.config()
+dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI, {
-  dbName: "URL_SHORTENER"
-}).then(() => console.log("MongoDB connected")).catch((error) => console.log(error));
-
-const app = express()
-
-app.use(express.urlencoded({extended: true}))
-app.use(express.static("public"))
-
-const port = process.env.PORT || 4000
-
-app.set("view engine", "ejs")
-app.set("views", "./views")
-app.use(express.static("public"))
-
-// rendering the ejs file
-app.get('/', (req,res) => {
-  res.render("index.ejs", {shortURL: null})
+  dbName: 'URL_SHORTENER'
 })
+.then(() => console.log("MongoDB connected"))
+.catch((error) => console.log(error));
 
-// shortening logic
-app.post('/short', shortURL)
+const app = express();
 
-// redirect to original URL using short code
-app.get('/:shortCode', getOriginalURL)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
-app.listen(port, () => console.log(`Server is running on port : ${port}`))
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.render("index.ejs", { shortURL: null });
+});
+
+app.post('/short', shortURL);
+app.get('/:shortCode', getOriginalURL);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
